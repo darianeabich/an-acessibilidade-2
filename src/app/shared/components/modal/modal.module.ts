d781cf3720828c121871ab217ab2dfa274800942
Dariane Abich <darianeabich@gmail.com>
2021-06-15 10:31:52 -0300

    Diretiva FocusTrap: iniciando o modal com focus, utilização das teclas Tab e Shift+Tab para navegar somente dentro do modal e ESC para fechar

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
 
@@ -12,6 +14,7 @@ import { ModalService } from './services/modal.service';
   ],
   imports: [
     CommonModule,
+      FocusTrapModule,
   ],
   providers: [
     ModalService
