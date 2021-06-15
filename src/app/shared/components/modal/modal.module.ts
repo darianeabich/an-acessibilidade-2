efc089b5a7093716e8a258dee28e606a1844c37c
Dariane Abich <darianeabich@gmail.com>
2021-06-15 11:04:20 -0300

    Diretiva FocusBack

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 
 import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from './modal.component';
@@ -14,7 +15,8 @@ import { ModalService } from './services/modal.service';
   ],
   imports: [
     CommonModule,
-      FocusTrapModule,
+    FocusTrapModule,
+    FocusBackModule,
   ],
   providers: [
     ModalService
